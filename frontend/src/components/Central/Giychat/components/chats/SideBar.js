8467f6dd63dcf7587b03046e971cd53af157c88e
Inomzhon Ibragimov <mail4inom@Inomzhons-MacBook-Pro.local>
2018-11-06 21:08:09 +0500

    j

--- frontend/src/components/Central/Giychat/components/chats/SideBar.js
+++ frontend/src/components/Central/Giychat/components/chats/SideBar.js
@@ -16,10 +16,10 @@ export default class SideBar extends Component {
 						var num = Math.floor((Math.random() * colors.length));
 						var color = colors[num];
 						return (
-							<ul className="list-unstyled">
-								<li key={Math.random()} className="left clearfix">
+							<ul key={Math.random()} className="list-unstyled">
+								<li  className="left clearfix">
 									<span className="chat-img pull-left">
-										<img src='https://scontent-frt3-2.cdninstagram.com/vp/3ca2fb4fd8fb9e90234d42da07f63fed/5C505CC9/t51.2885-15/e35/41349438_869720689897963_6915892188155667819_n.jpg' alt="User Avatar" class="img-circle" />
+										<img src='https://scontent-frt3-2.cdninstagram.com/vp/3ca2fb4fd8fb9e90234d42da07f63fed/5C505CC9/t51.2885-15/e35/41349438_869720689897963_6915892188155667819_n.jpg' alt="User Avatar" className="img-circle" />
 									</span>
 									<div className="chat-body clearfix">
 										<div className="header_sec ">
@@ -33,7 +33,7 @@ export default class SideBar extends Component {
 				</div>
 				<div className="current-user">
 					<div onClick={() => { logout() }} title="Logout" className="logout">
-						<button className='btn btn-primary form-control'>чатдан чиқиш &nbsp;<i class="fa fa-sign-out" aria-hidden="true"></i></button>
+						<button className='btn btn-primary form-control'>чатдан чиқиш &nbsp;<i className="fa fa-sign-out" aria-hidden="true"></i></button>
 					</div>
 				</div>
 			</div>
--- frontend/src/components/Central/Giychat/components/messages/Messages.js
+++ frontend/src/components/Central/Giychat/components/messages/Messages.js
@@ -34,7 +34,7 @@ export default class Messages extends Component {
 									className={`message-container ${mes.sender === user.name && 'right'}`}
 								>
 									<div className="message my-message">
-										<img alt="" class="img-circle medium-image" src='https://scontent-frt3-2.cdninstagram.com/vp/3ca2fb4fd8fb9e90234d42da07f63fed/5C505CC9/t51.2885-15/e35/41349438_869720689897963_6915892188155667819_n.jpg' />
+										<img alt="" className="img-circle medium-image" src='https://scontent-frt3-2.cdninstagram.com/vp/3ca2fb4fd8fb9e90234d42da07f63fed/5C505CC9/t51.2885-15/e35/41349438_869720689897963_6915892188155667819_n.jpg' />
 										<div className="message-body">
 											<div className="message-body-inner">
 												<div className="message-info">
