058c02c4f1269469519bb7f6481d378997ec3723
hackrack <mail4umed@yahoo.com>
2018-11-21 12:37:07 -0500

    heroku version

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../backend/frontend/build/index.html'));
+  res.sendFile(path.join('../backend/frontend/build/index.html'));
 });
 
 
