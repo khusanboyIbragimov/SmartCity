36dbf662d4dd4bf9e64ba16f0b6f920a8f248de0
hackrack <mail4umed@yahoo.com>
2018-11-24 23:35:12 -0500

    heroku version

--- frontend/src/components/Central/Giychat/components/Layout.js
+++ frontend/src/components/Central/Giychat/components/Layout.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { USER_CONNECTED, LOGOUT } from '../Events';
 import LoginForm from './LoginForm';
 import ChatContainer from './chats/ChatContainer';
-const socketUrl = 'http://localhost:3100/';
+const socketUrl = 'https://g-smartcity.herokuapp.com/socket.io/socket.io.js';
 // const socketUrl = 'http://192.168.43.95:3100/';
 var socket = io(socketUrl);
 
